a03c5ebf0c476193078047d299b60d64b48c9f46
Yornie Westink <jinxie88@live.nl>
2017-03-06 16:01:44 +0100

    Code conventions

---
+++ Assets/Scripts/CodeConventions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeConventions : MonoBehaviour
+{
+    private float _testVar; //vars with "_" + CamelCase
+    public const float TEST_CONST = 0; //Constants full Caps with a underscore spacing
+
+    private delegate void DelegateConvention(); //CamelCase met eerste letterCaps
+
+    //properties are methods are the only ones allowed to be public
+    public object X
+    {
+        get
+        {
+            return null;
+        }
+        set
+        {
+
+        }
+    }
+
+
+
+
+}
